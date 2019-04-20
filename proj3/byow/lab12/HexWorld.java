0030c4536719b4b1a545b2a361d86a84a7e9c8c8
Martin <leemartin1208@berkeley.edu>
2019-04-19 18:29:43 -0700

    Added poj3 to the folder

---
+++ proj3/byow/lab12/HexWorld.java
@@ -0,0 +1,101 @@
+package byow.lab12;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import byow.TileEngine.TERenderer;
+import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
+
+import javax.swing.text.Position;
+import java.util.Random;
+
+/**
+ * Draws a world consisting of hexagonal regions.
+ */
+public class HexWorld {
+
+    static Random RANDOM = new Random();
+
+    public HexWorld(){
+
+    }
+    public static int hexRowWidth(int s, int i) {
+        int effectiveI = i;
+        if (i >= s) {
+            effectiveI = 2 * s - 1 - effectiveI;
+        }
+
+        return s + 2 * effectiveI;
+    }
+
+    public static void addRow(TETile[][] world, Position p, int width, TETile t) {
+        for (int xi = 0; xi < width; xi += 1) {
+            int xCoord = p.x + xi;
+            int yCoord = p.y;
+            world[xCoord][yCoord] = TETile.colorVariant(t, 32, 32, 32, RANDOM);
+        }
+    }
+    public static int hexRowOffset(int s, int i) {
+        int effectiveI = i;
+        if (i >= s) {
+            effectiveI = 2 * s - 1 - effectiveI;
+        }
+        return -effectiveI;
+    }
+    public static void addHexagon(TETile[][] world, Position p, int s, TETile t) {
+
+        if (s < 2) {
+            throw new IllegalArgumentException("Hexagon must be at least size 2.");
+        }
+
+        // hexagons have 2*s rows. this code iterates up from the bottom row,
+        // which we call row 0.
+        for (int yi = 0; yi < 2 * s; yi += 1) {
+            int thisRowY = p.y + yi;
+
+            int xRowStart = p.x + hexRowOffset(s, yi);
+            Position rowStartP = new Position(xRowStart, thisRowY);
+
+            int rowWidth = hexRowWidth(s, yi);
+
+            addRow(world, rowStartP, rowWidth, t);
+
+        }
+    }
+    private static class Position {
+        private int x;
+        private int y;
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    @Test
+    public void testHexRowWidth() {
+        assertEquals(3, hexRowWidth(3, 5));
+        assertEquals(5, hexRowWidth(3, 4));
+        assertEquals(7, hexRowWidth(3, 3));
+        assertEquals(7, hexRowWidth(3, 2));
+        assertEquals(5, hexRowWidth(3, 1));
+        assertEquals(3, hexRowWidth(3, 0));
+        assertEquals(2, hexRowWidth(2, 0));
+        assertEquals(4, hexRowWidth(2, 1));
+        assertEquals(4, hexRowWidth(2, 2));
+        assertEquals(2, hexRowWidth(2, 3));
+    }
+
+    @Test
+    public void testHexRowOffset() {
+        assertEquals(0, hexRowOffset(3, 5));
+        assertEquals(-1, hexRowOffset(3, 4));
+        assertEquals(-2, hexRowOffset(3, 3));
+        assertEquals(-2, hexRowOffset(3, 2));
+        assertEquals(-1, hexRowOffset(3, 1));
+        assertEquals(0, hexRowOffset(3, 0));
+        assertEquals(0, hexRowOffset(2, 0));
+        assertEquals(-1, hexRowOffset(2, 1));
+        assertEquals(-1, hexRowOffset(2, 2));
+        assertEquals(0, hexRowOffset(2, 3));
+    }
+}
