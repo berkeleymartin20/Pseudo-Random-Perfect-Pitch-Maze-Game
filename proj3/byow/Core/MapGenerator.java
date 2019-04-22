912218d124406e02e93f54d039b0e94fd6d9168f
Claire Tsau <clairetsau@berkeley.edu>
2019-04-21 22:41:58 -0700

    weightedRandomDirection

--- proj3/byow/Core/MapGenerator.java
+++ proj3/byow/Core/MapGenerator.java
@@ -3,6 +3,8 @@ package byow.Core;
 import byow.TileEngine.TETile;
 import byow.TileEngine.Tileset;
 
+import java.util.Random;
+
 /**
  * @author Martin Lee and Claire Tsau
  * @Version 1.0 4/21/19
@@ -15,6 +17,8 @@ public class MapGenerator {
     TETile[][] world;
     //keeps track of connected pixels/blocks
     UnionFind path;
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED); //dunno how to deal with seed
 
     /**
      * @param width  The width of the screen
@@ -23,6 +27,7 @@ public class MapGenerator {
     public MapGenerator(int width, int length) {
         WIDTH = width;
         LENGTH = length;
+
 
         world = new TETile[WIDTH][LENGTH];
         path = new UnionFind(WIDTH*LENGTH);
@@ -40,8 +45,8 @@ public class MapGenerator {
      * x = 0 y = 0 is the top left corner of the screen
      * This will be used to smooth operations between world and path smoother
      *
-     * @param x x coordiante
-     * @param y y coordiante
+     * @param x x coordinate
+     * @param y y coordinate
      * @return returns index of the position. e.g.) 3x3 grid is [[0,1,2],[3,4,5],[6,7,8]]
      */
     public int convertXYtoIndex(int x, int y) {
@@ -65,6 +70,25 @@ public class MapGenerator {
         coord[0] = p % WIDTH;
         coord[1] = p / WIDTH;
         return coord;
+    }
+
+    /**
+     * randomly selects one of four directions, with one of them being weighted
+     * the weighted direction will have weight = 2
+     * the others have weight = 1
+     *
+     * @param up,down,left,right, in that order, weights/frequencies of each direction (may be 0)
+     * @return "up", "down", "left", or "right"
+     */
+    private String weightedRandomDirection(int up, int down, int left, int right){
+        int[] frequencies = new int[]{up,down,left,right};
+        int result = RandomUtils.discrete(RANDOM, frequencies);
+        switch (result) {
+            case 0: return "up";
+            case 1: return "down";
+            case 2: return "left";
+            case 4: return "right";
+        }
     }
 
 }
